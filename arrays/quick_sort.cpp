ca1ad112baba400c14b8df4e64e2eb39cb2f0317
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-10-20 23:46:09 +0530

    Refactor array elements in quick_sort.cpp

--- arrays/quick_sort.cpp
+++ arrays/quick_sort.cpp
@@ -66,7 +66,7 @@ int main(){
     // }
     cout<<endl;
     SolutionWithArray s1;
-    int arr[] = {10,9,8,7,6,5,4,3,2,1};
+    int arr[] = {10,9,-9,7,6,50,4,3,2,1};
     int n = sizeof(arr)/sizeof(arr[0]);
     s1.quickSort(arr,n);
     for(int i=0;i<n;i++){
