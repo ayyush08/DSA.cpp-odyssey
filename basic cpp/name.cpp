2c8be832b81d4b70663bef742fd35cdfb8654d6e
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-10-23 23:09:36 +0530

    Refactor code to count the number of words in a sentence

--- basic cpp/name.cpp
+++ basic cpp/name.cpp
@@ -4,23 +4,46 @@
 //     cout << "Hey Curator!" << endl << "Hey Ayush";
 //     cout << "Hey Curator!" << endl << "Hey Ayush";
 // }
-#include <iostream>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
+
+int wordCount(string sent){
+    int count = 1;
+
+    for(int i = 0; i < sent.size(); i++)
+    {
+        if(sent[i] == ' ')
+        {
+            count++;
+        }
+    }
+
+    return (count);
+}
+
+vector<string> func(vector<string> sent)
+{
+    int s = sent.size();
+
+    vector<string> result;
+    for (int i = 0; i < s; i++)
+    {   
+        int count = wordCount(sent[i]);
+        if(count ==1) result.push_back(sent[i]);
+    }
+
+    return result;
+    
+}
 
 int main()
 {
-    int arr[] = {10, 20, 30, 30, 30, 20};
-    map<int, int> Freqtable;
-    for (int i = 0; i < 6; i++)
+    vector<string> names = {"ayush kumar", "ayush", "ayush kumar"};
+    vector<string> res = func(names);
+    for(int i = 0; i < res.size(); i++)
     {
-        Freqtable[arr[i]]++;
+        cout << res[i] << endl;
     }
-    map<int, int>::iterator it;
-    for (it = Freqtable.begin();it != Freqtable.end(); it++)
-    {
-        cout << it->first << " : " << it->second << endl;
-    }
-
+    
     return 0;
 }
