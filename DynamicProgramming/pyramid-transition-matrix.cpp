f993c6fc9385df4c203b628282a7eeb58953f084
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-12-30 23:25:36 +0530

    Pyramid Transition Matrix and Water Jug Problem

---
+++ DynamicProgramming/pyramid-transition-matrix.cpp
@@ -0,0 +1,73 @@
+// 756. Pyramid Transition Matrix
+// You are stacking blocks to form a pyramid. Each block has a color, which is represented by a single letter. Each row of blocks contains one less block than the row beneath it and is centered on top.
+
+// To make the pyramid aesthetically pleasing, there are only specific triangular patterns that are allowed. A triangular pattern consists of a single block stacked on top of two blocks. The patterns are given as a list of three-letter strings allowed, where the first two characters of a pattern represent the left and right bottom blocks respectively, and the third character is the top block.
+
+// For example, "ABC" represents a triangular pattern with a 'C' block stacked on top of an 'A' (left) and 'B' (right) block. Note that this is different from "BAC" where 'B' is on the left bottom and 'A' is on the right bottom.
+// You start with a bottom row of blocks bottom, given as a single string, that you must use as the base of the pyramid.
+
+// Given bottom and allowed, return true if you can build the pyramid all the way to the top such that every triangular pattern in the pyramid is in allowed, or false otherwise.
+
+ 
+
+// Example 1:
+
+
+// Input: bottom = "BCD", allowed = ["BCC","CDE","CEA","FFF"]
+// Output: true
+// Explanation: The allowed triangular patterns are shown on the right.
+// Starting from the bottom (level 3), we can build "CE" on level 2 and then build "A" on level 1.
+// There are three triangular patterns in the pyramid, which are "BCC", "CDE", and "CEA". All are allowed.
+// Example 2:
+
+
+// Input: bottom = "AAAA", allowed = ["AAB","AAC","BCD","BBE","DEF"]
+// Output: false
+// Explanation: The allowed triangular patterns are shown on the right.
+// Starting from the bottom (level 4), there are multiple ways to build level 3, but trying all the possibilites, you will get always stuck before building level 1.
+ 
+
+// Constraints:
+
+// 2 <= bottom.length <= 6
+// 0 <= allowed.length <= 216
+// allowed[i].length == 3
+// The letters in all input strings are from the set {'A', 'B', 'C', 'D', 'E', 'F'}.
+// All the values of allowed are unique.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    unordered_map<string,bool>dp;
+    bool solve(string bottom,int i,string above,unordered_map<string,vector<char>>& mp){
+        if(bottom.size()==1) return true;
+         string key = bottom + "_" + to_string(i) + "_" + above;
+        if(dp.count(key)) return dp[key];
+
+        if(i==bottom.size()-1) return dp[key] = solve(above,0,"",mp);
+
+        string pair = bottom.substr(i,2);
+        if(!mp.count(pair)) return dp[key] = false;
+
+        for(auto& c:mp[pair]){
+            above.push_back(c); //try
+            if(solve(bottom,i+1,above,mp)==true) return dp[key] = true; //explore
+            above.pop_back(); //undo
+        }
+        return dp[key] = false;
+    }
+    bool pyramidTransition(string bottom, vector<string>& allowed) {
+        unordered_map<string,vector<char>>mp;
+        for(string block:allowed){
+            mp[block.substr(0,2)].push_back(block[2]);
+        }
+
+        return solve(bottom,0,"",mp);
+
+    }
+};
+
+//Time Complexity: O(N^2 * 3^N) where N is the length of the bottom string. In the worst case, for each pair of blocks, there could be up to 3 possible blocks that can be placed on top, leading to an exponential number of combinations.
+//Space Complexity: O(N^2 * 3^N) for the memoization map
---
+++ Graphs/water-jug-problem.cpp
@@ -0,0 +1,85 @@
+// 365. Water and Jug Problem
+// You are given two jugs with capacities x liters and y liters. You have an infinite water supply. Return whether the total amount of water in both jugs may reach target using the following operations:
+
+// Fill either jug completely with water.
+// Completely empty either jug.
+// Pour water from one jug into another until the receiving jug is full, or the transferring jug is empty.
+
+// Example 1:
+
+// Input: x = 3, y = 5, target = 4
+
+// Output: true
+
+// Explanation:
+
+// Follow these steps to reach a total of 4 liters:
+
+// Fill the 5-liter jug (0, 5).
+// Pour from the 5-liter jug into the 3-liter jug, leaving 2 liters (3, 2).
+// Empty the 3-liter jug (0, 2).
+// Transfer the 2 liters from the 5-liter jug to the 3-liter jug (2, 0).
+// Fill the 5-liter jug again (2, 5).
+// Pour from the 5-liter jug into the 3-liter jug until the 3-liter jug is full. This leaves 4 liters in the 5-liter jug (3, 4).
+// Empty the 3-liter jug. Now, you have exactly 4 liters in the 5-liter jug (0, 4).
+// Reference: The Die Hard example.
+
+// Example 2:
+
+// Input: x = 2, y = 6, target = 5
+
+// Output: false
+
+// Example 3:
+
+// Input: x = 1, y = 2, target = 3
+
+// Output: true
+
+// Explanation: Fill both jugs. The total amount of water in both jugs is equal to 3 now.
+
+// Constraints:
+
+// 1 <= x, y, target <= 103
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    set<pair<int, int>> visited;
+    queue<pair<int, int>> q;
+    void checkAndInsert(int a, int b)
+    {
+        if (!visited.count({a, b}))
+        {
+            visited.insert({a, b});
+            q.push({a, b});
+        }
+    }
+    bool canMeasureWater(int x, int y, int target)
+    {
+        q.push({0, 0});
+        visited.insert({0, 0});
+
+        while (q.size())
+        {
+            int a = q.front().first;
+            int b = q.front().second;
+            q.pop();
+
+            if (a == target || b == target || a + b == target)
+                return true;
+            checkAndInsert(a, 0);
+            checkAndInsert(0, b);
+            checkAndInsert(x, b);
+            checkAndInsert(a, y);
+            int transferAtoB = min(a, y - b);
+            checkAndInsert(a - transferAtoB, b + transferAtoB);
+            int transferBtoA = min(x - a, b);
+            checkAndInsert(a + transferBtoA, b - transferBtoA);
+        }
+        return false;
+    }
+};
