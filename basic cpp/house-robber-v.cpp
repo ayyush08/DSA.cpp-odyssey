e6e33edd7b84d9882c97f7733f3e10ee70135e54
AYUSH KUMAR GUPTA <144051175+ayyush08@users.noreply.github.com>
2026-02-18 10:47:28 +0530

    extension-testing-4

---
+++ basic cpp/house-robber-v.cpp
@@ -0,0 +1,51 @@
+// House Robber V
+
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed and is protected by a security system with a color code.
+// You are given two integer arrays nums and colors , both of length n , where nums[i] is the amount of money in the i th house and colors[i] is the color code of that house.
+// You cannot rob two adjacent houses if they share the same color code.
+// Return the maximum amount of money you can rob.
+// Example 1:
+// Input: nums = [1,4,3,5], colors = [1,1,2,2]
+// Output: 9
+// Explanation:
+// Choose houses i = 1 with nums[1] = 4 and i = 3 with nums[3] = 5 because they are non-adjacent.
+// Thus, the total amount robbed is 4 + 5 = 9 .
+// Example 2:
+// Input: nums = [3,1,2,4], colors = [2,3,2,2]
+// Output: 8
+// Explanation:
+// Choose houses i = 0 with nums[0] = 3 , i = 1 with nums[1] = 1 , and i = 3 with nums[3] = 4 .
+// This selection is valid because houses i = 0 and i = 1 have different colors, and house i = 3 is non-adjacent to i = 1 .
+// Thus, the total amount robbed is 3 + 1 + 4 = 8 .
+// Example 3:
+// Input: nums = [10,1,3,9], colors = [1,1,1,2]
+// Output: 22
+// Explanation:
+// Choose houses i = 0 with nums[0] = 10 , i = 2 with nums[2] = 3 , and i = 3 with nums[3] = 9 .
+// This selection is valid because houses i = 0 and i = 2 are non-adjacent, and houses i = 2 and i = 3 have different colors.
+// Thus, the total amount robbed is 10 + 3 + 9 = 22 .
+// Constraints:
+// 1 <= n == nums.length == colors.length <= 10 5
+// 1 <= nums[i], colors[i] <= 10 5
+
+class Solution {class Solution {
+public:public:
+    vector<long long>dp;    vector<long long>dp;
+    vector<int>colorCodes;    vector<int>colorCodes;
+    int n;    int n;
+    long long func(vector<int>& nums, int i){    long long func(vector<int>& nums, int i){
+        if(i<0) return 0;        if(i<0) return 0;
+        if(i==0) return nums[0];        if(i==0) return nums[0];
+        if(dp[i]!=-1) return dp[i];        if(dp[i]!=-1) return dp[i];
+
+        long long rob = nums[i];        long long rob = nums[i];
+        if(colorCodes[i]!=colorCodes[i-1]) rob+=func(nums,i-1);        if(colorCodes[i]!=colorCodes[i-1]) rob+=func(nums,i-1);
+        else rob+=func(nums,i-2); //matching code        else rob+=func(nums,i-2); //matching code
+        long long dontRob = func(nums,i-1);        long long dontRob = func(nums,i-1);
+        return dp[i]  = max(rob,dontRob);        return dp[i]  = max(rob,dontRob);
+                
+    }    }
+    long long rob(vector<int>& nums, vector<int>& colors) {    long long rob(vector<int>& nums, vector<int>& colors) {
+        n = nums.size();        n = nums.size();
+        dp.assign(n,-1);        dp.assign(n,-1);
+        colorCodes = colors;        colorCodes = colors;
